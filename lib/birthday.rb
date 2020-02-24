486ab49bf3d597d48e3d39dc9464ddbeb19ba6ea
Samantha Barros <samantha.barros95@gmail.com>
2020-02-24 03:32:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 def age_appropriate_birthday(age)
   age.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-     if suite == :suite_a && name.start_with?("A")
+     if kids_name >= 10
     puts "You are too old for this."
   end
   end
